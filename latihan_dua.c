d3bc51bfa0907705af5921bbd23ddc359dfb895f
Poltepop <dewaputuadhitya@gmail.com>
2026-03-30 16:46:04 +0700

    fix latihan 2

--- latihan_dua.c
+++ latihan_dua.c
@@ -5,10 +5,10 @@ int main(){
     int ganjil;
     int n = 8;
 
-    for(ganjil = 1; ganjil < n; ganjil += 2){
-        jumlah = jumlah + ganjil;
-        printf("Jumlah bilangan ganjil positif kurang dari %d is %d\n", n, ganjil);
-    }
+    for(ganjil = 1; ganjil < n; ganjil += 2)
+    
+    jumlah = jumlah + ganjil;
+    printf("Jumlah bilangan ganjil positif kurang dari %d is %d\n", n, ganjil);
 
     return 0;
 }
